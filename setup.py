0f946fb19de6bb5c22a483c233290d775f923db3
sprayidle <sprayidle@gmail.com>
2015-12-04 12:51:49 +1300

    First try

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='utloader',
+      version='0.0.1',
+      description='utloader',
+      author='Spray Idle',
+      author_email='sprayidle@gmail.com',
+      url='',
+      install_requires=['Flask==0.10.1',
+						'requests>=2.7.0',
+						],
+     )
